a9195dfbd9dc193ad527907b2f38e69adbce7457
IIITManjeet <manjeetpathak2003@gmail.com>
2023-09-06 13:56:45 +0530

    tempu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { About } from "./components/About";
 import { MastHead } from "./components/MastHead";
 import { Navbar } from "./components/Navbar";
 import { ScrollBar } from "./components/ScrollBar";
@@ -7,9 +8,12 @@ function App() {
     <div className="flex flex-col relative min-h-screen">
       <div className="fixed rounded-full bg-[#D41EF1B0] blur-[150px] opacity-[0.30] z-0 right-0 top-[-220px] h-[400px] w-[400px] absolute" />
       <div className="fixed rounded-[400px] bg-[#D41EF1B0] blur-[200px] opacity-[0.30] top-[300px] left-[-150px] h-[400px] w-[400px] absolute" />
-      <Navbar />
-      <MastHead />
-      <ScrollBar />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <MastHead />
+        <ScrollBar />
+      </div>
+      <About />
     </div>
   );
 }
